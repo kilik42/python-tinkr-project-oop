8a25ebadb3c42ae2c1eedf4801d2d56e40e453e9
marvin evins <maevins@cps.edu>
2020-05-30 20:17:53 -0500

    working on initial ui

---
+++ script1.py
@@ -0,0 +1,46 @@
+from tkinter import *
+
+window= Tk()
+
+
+l1 = Label(window, text ="Title")
+l1.grid(row=0, column=0)
+
+
+l2 = Label(window, text ="Author")
+l2.grid(row=0, column=2)
+
+
+l3 = Label(window, text ="Year")
+l3.grid(row=1, column=0)
+
+
+l4 = Label(window, text ="ISBN")
+l4.grid(row=1, column=2)
+
+#title entry field
+title_text = StringVar()
+e1 = Entry(window, textvariable=title_text)
+e1.grid(row=0, column=1)
+
+#year entry field
+year_text = StringVar()
+y1 = Entry(window, textvariable=year_text)
+y1.grid(row=1, column=1)
+
+#author entry field
+author_text = StringVar()
+a1 = Entry(window, textvariable=author_text)
+a1.grid(row=0, column=3)
+
+
+
+#isbn entry field
+isbn_text = StringVar()
+isbn1 = Entry(window, textvariable=isbn_text)
+isbn1.grid(row=1, column=3)
+
+
+
+
+window.mainloop()
